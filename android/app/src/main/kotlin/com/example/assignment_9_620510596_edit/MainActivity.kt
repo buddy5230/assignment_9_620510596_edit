ce2a8292b3de6c48f65fcb8b10490f5303358596
buddy5230 <74503111+buddy5230@users.noreply.github.com>
2023-02-28 19:21:44 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/assignment_9_620510596_edit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.assignment_9_620510596_edit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'video_play.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+final List<Map<String, dynamic>> _videos = [
+  {      'title': 'Big Buck Bunny',
+          'url': 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+          'thumbnail': 'lib/images/bigbuckbunny.jpg',
+          'user':'UserA',
+          'description':'58k views . 6 hours ago',
+  },
+  {      'title': 'Elephant Dream',
+          'url': 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+          'thumbnail': 'lib/images/elephants_dream1.jpg',
+          'user':'UserB',
+          'description':'100k views . 1 years ago',
+  },
+  {      'title': 'For Bigger Blazes',
+          'url': 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
+          'thumbnail': 'lib/images/forbiggerblazes.jpg',
+          'user':'UserC',
+          'description':'45k views . 2 days ago',
+  },
+  {      'title': 'For Bigger Escape',
+          'url': 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
+          'thumbnail': 'lib/images/forbigescape.jpg',
+          'user':'UserD',
+          'description':'6.2k views . 10 hours ago',
+  },
+  {      'title': 'For Bigger Fun',
+          'url': 'lib/images/elephants_dream1.jpg',
+          'thumbnail': 'lib/images/forbiggerfun.jpg',
+          'user':'UserE',
+          'description':'10k views . 4 months ago',
+  },
+];
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: ListView.builder(
+        itemCount: _videos.length,
+        itemBuilder: (context, index) {
+          final video = _videos[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => VideoPlayerScreen(
+                    title: video['title'],
+                    url: video['url'],
+                  ),
+                ),
+              );
+            },
+            child: Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Image.network(
+                    video['thumbnail'],
+                    fit: BoxFit.cover,
+                    height: 200,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          video['title'],
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Description: ${video['description']}',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(height: 8),
+                        Text.rich(
+                          TextSpan(
+                            children: [
+                              TextSpan(
+                                text: video['user'],
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
